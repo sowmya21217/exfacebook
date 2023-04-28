7b571688909a981bbd110c4ab75cea0e64488b9e
sowmya <k.l.sowmya219@gmail.com>
2023-04-28 14:06:22 +0530

    adding user

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
+import { verifyToken } from "./middleware/auth.js";
 
 const __filename =fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
